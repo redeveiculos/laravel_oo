e9f8012fc383e67380410a5eac1edacb1971f059
redeveiculos <marcelo.soares@redeveiculos.com>
2022-01-10 09:09:40 -0300

    teste chat-api

--- index.php
+++ index.php
@@ -5,6 +5,22 @@
 echo "Hello World!";
 echo "<br />";
 echo "Yes";
+echo "<br />";
+echo "<br />";
+
+$json = @file_get_contents("https://api.chat-api.com/instance82080/status?token=kaft4m7wce0nlia9");
+
+if ($json) {
+    $data = json_decode($json, FALSE);
+
+    if ($data && $data->accountStatus && $data->accountStatus == "authenticated") {
+        echo "OK --> $json";
+    } else {
+        echo "ALARME";
+    }
+} else {
+    echo "ALARME";
+}
 
 
 ?>
